365f70d9673b6eec5e28a1834d9b91f1dae2dfa4
Adigun574 <adigunibrahim574@gmail.com>
2019-12-06 18:56:25 -0800

    cors

--- index.js
+++ index.js
@@ -69,8 +69,11 @@ app.use('/accounts',accounts)
 app.use('/transactions',transactions)
 
 app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    // res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token")
     next()
 });
 
