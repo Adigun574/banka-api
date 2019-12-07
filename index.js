ebc0cc2e9bdcf93f390bccb7fd895ac93a0f49a9
Adigun574 <adigunibrahim574@gmail.com>
2019-12-06 18:07:30 -0800

    cors

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(cors())
 app.all('/', function(req, res, next) {
-    //res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
 });
