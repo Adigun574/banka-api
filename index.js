6eb9f0a423d4ea9c6e81c51fd6cbe4a72f657d28
Adigun574 <adigunibrahim574@gmail.com>
2019-12-07 07:54:52 -0800

    cors

--- index.js
+++ index.js
@@ -68,11 +68,11 @@ app.use(bodyParser.json())
 //     next()   
 // });
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 //app.options('*', cors())
 
