2e34e9b564a80fe05dabfcf01aff397bc2cce71f
Adigun574 <adigunibrahim574@gmail.com>
2019-12-07 09:05:58 -0800

    cors

--- index.js
+++ index.js
@@ -47,10 +47,10 @@ async function execute(){
 const app = express()
 const port = process.env.PORT || 3000
 
-var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200
-  }
+// var corsOptions = {
+//     origin: '*',
+//     optionsSuccessStatus: 200
+//   }
 
 
 //app.use(cors(corsOptions))
@@ -69,11 +69,11 @@ app.use(bodyParser.json())
 //     next()   
 // });
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 // app.options('*', cors())
 
